a29db86e7f215ebd65701de35d7442bc099ac51e
Khaleed <saludeenkhaleed13@gmail.com>
2025-10-16 19:04:48 +0100

    first commit

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ const app = express()
 // Connect Database
 await connectDB();
 
+// Routes
+app.get('/', (req,res) => {
+    res.json({ message: "CarrerPay API is running and in Development" })
+})
+
+
+// PORT
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.get('/', (req,res) => {
-    res.send(`Server is Running on ${PORT}`)
-})
+
